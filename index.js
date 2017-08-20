c68699ec2e5ea7242d312b20574c8f5d985ec2ff
Alonso Salcido <alonsosalcido13@gmail.com>
2017-08-20 00:10:29 -0600

    v-for and binding data in properties and tags

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = new Vue({
     data: {
         mostrar: true,
         message: 'Hola Vue!',
-        imagen: "http://laravelacademy.org/wp-content/uploads/2016/08/00-featured-vuejs-logo-simple-256x128.jpg"
+        imagen: "http://laravelacademy.org/wp-content/uploads/2016/08/00-featured-vuejs-logo-simple-256x128.jpg",
+        cursos: [
+            { name: "Fundamentos de React", url: 'http://cursos.carlosazaustre.es/p/react-js' },
+            { name: "Redux con React", url: 'http://cursos.carlosazaustre.es/p/curso-profesional-de-redux-y-react' },
+            { name: "React Native", url: 'http://cursos.carlosazaustre.es/p/react-native' },
+          ]
     },
     methods: {
         toggleMostrar: function(){
