4f137f0876d4aea2cc959769f15fe5b676fb486b
Alonso Salcido <alonsosalcido13@gmail.com>
2017-08-20 00:02:50 -0600

    templates in code, adding toggle function, using v-if directive and v-on:click abbreviated @click

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const app = new Vue({
     el: '#app',
+    template: `
+    <div>
+        <div v-if="mostrar">
+        {{ message }}
+        <img :src="imagen" />
+        </div>
+        <button @click="toggleMostrar">Mostrar/Ocultar</button>
+    </div>
+    `,
     data: {
-        message: 'Hola Vue!'
+        mostrar: true,
+        message: 'Hola Vue!',
+        imagen: "http://laravelacademy.org/wp-content/uploads/2016/08/00-featured-vuejs-logo-simple-256x128.jpg"
+    },
+    methods: {
+        toggleMostrar: function(){
+            this.mostrar = !this.mostrar
+        }
     }
 })
